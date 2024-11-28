bc69213cb03f870a5e408613c5ca33bc307c7846
Maryam Abloua <maryam.ablouade@hes-so.ch>
2024-11-28 16:51:43 +0100

    Add CSS for LoginPage design

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,15 +1,16 @@
+import '../styles/LoginPage.css'
+
 const LoginPage = () => {
-    return (
-      <div>
-        <h1>Connexion</h1>
-        <form>
-          <input type="email" placeholder="Email" />
-          <input type="password" placeholder="Mot de passe" />
-          <button type="submit">Se connecter</button>
-        </form>
-      </div>
-    )
-  }
-  
-  export default LoginPage
-  
+  return (
+    <div className="login-container">
+      <h1>Connexion</h1>
+      <form>
+        <input type="email" className="input-field" placeholder="Email" />
+        <input type="password" className="input-field" placeholder="Mot de passe" />
+        <button type="submit" className="login-button">Se connecter</button>
+      </form>
+    </div>
+  )
+}
+
+export default LoginPage
