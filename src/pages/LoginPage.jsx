1dec1dbfb48f2f011bf757f058e0d7fcb531f1f6
Maryam Abloua <maryam.ablouade@hes-so.ch>
2024-11-28 16:17:22 +0100

    Added static UI : LoginPage

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,7 +1,12 @@
 const LoginPage = () => {
     return (
       <div>
-        <h1>Page de Connexion</h1>
+        <h1>Connexion</h1>
+        <form>
+          <input type="email" placeholder="Email" />
+          <input type="password" placeholder="Mot de passe" />
+          <button type="submit">Se connecter</button>
+        </form>
       </div>
     )
   }
